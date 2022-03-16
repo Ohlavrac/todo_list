66541a56b7338c872ed4bd2a328ff9736f0e5501
Ohlavrac <joaovicrcarvalho@gmail.com>
2022-03-16 11:15:34 -0300

    added text styles

---
+++ lib/shared/theme/text_styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextStyles {
+  static final subtitle = GoogleFonts.barlow(
+    fontSize: 24,
+    fontWeight: FontWeight.w500
+  );
+
+  static final barText = GoogleFonts.barlow(
+    fontSize: 26,
+    fontWeight: FontWeight.w500
+  );
+
+  static final simpleText = GoogleFonts.barlow(
+    fontSize: 24,
+    fontWeight: FontWeight.w500
+  );
+}
