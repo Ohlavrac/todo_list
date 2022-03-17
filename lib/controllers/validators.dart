df0dedf22eebefcab252fae64132cca8a4d99fbb
Ohlavrac <joaovicrcarvalho@gmail.com>
2022-03-17 11:24:52 -0300

    added new validator

--- lib/controllers/validators.dart
+++ lib/controllers/validators.dart
@@ -1,8 +1,16 @@
 class Validators {
   String? validateTitle (String? title) {
-    if (title == "") {
+    if (title == null || title.isEmpty) {
       return "Fill in the title field";
     }
+    return null;
+  }
+
+  String? validateDescription (String? description) {
+    if (description == null || description.isEmpty) {
+      return "Fill in the description field";
+    }
+
     return null;
   }
 }
